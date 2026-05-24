5602b109d28db0033f524ab455b360629f896813
buddhahh <matheushins2015@gmail.com>
2026-05-24 18:14:06 -0300

    inicio do moldes

--- me_ajuda_app/apps/usuarios/models.py
+++ me_ajuda_app/apps/usuarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    sobrenome = models.CharField('Sobrenome', max_length=50)
+    cpf = models.CharField('Cpf', max_length=11)
+    email = models.CharField('Email', max_length=50)
+
+
