88160262356d923c5a34fe4683b636d78ca70f1d
buddhahh <matheushins2015@gmail.com>
2026-05-24 19:38:13 -0300

    fim do models secretaria

--- me_ajuda_app/apps/secretarias/models.py
+++ me_ajuda_app/apps/secretarias/models.py
@@ -6,3 +6,11 @@ class Secretaria(models.Model):
     sigla = models.CharField('Sigla', max_length=10)
     descricao = models.CharField('Descricao', max_length=200)
     site = models.CharField('Site', max_length=200)
+
+    class Meta:
+        verbose_name = 'Secretaria'
+        verbose_name_plural = 'Secretarias'
+        ordering =['id']
+
+    def __str__(self):
+        return  f'{self.sigla} - {self.nome}'
