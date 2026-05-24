f89b038acf7b026001e68495d0d11961e75a83b4
Lucas <lucasso061204@gmail.com>
2026-05-24 16:33:52 -0300

    Ajustes em configurações e urls

--- me_ajuda_app/me_ajuda_app/settings.py
+++ me_ajuda_app/me_ajuda_app/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'me_ajuda_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,5 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'staticfiles'), 
+]
+
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
