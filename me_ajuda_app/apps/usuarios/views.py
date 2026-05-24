a377bd011ebd84bfb54ff5fc08764b5c1a92ff0c
buddhahh <matheushins2015@gmail.com>
2026-05-24 18:31:17 -0300

    fim views

--- me_ajuda_app/apps/usuarios/views.py
+++ me_ajuda_app/apps/usuarios/views.py
@@ -4,3 +4,6 @@ from rest_framework import viewsets
 from .serializer import UsuariosSerializer
 
 # Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer 
