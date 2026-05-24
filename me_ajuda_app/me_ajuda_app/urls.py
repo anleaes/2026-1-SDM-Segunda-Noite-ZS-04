21855555c24d79633a13844711e10b3915e673fd
buddhahh <matheushins2015@gmail.com>
2026-05-24 19:18:04 -0300

    ajuste do urls

--- me_ajuda_app/me_ajuda_app/urls.py
+++ me_ajuda_app/me_ajuda_app/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls', namespace='usuarios')),
+    path('servicos/', include('servicos.urls', namespace='servicos')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
