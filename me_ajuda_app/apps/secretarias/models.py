7a7f99b1a77d0af5d93eb959accccfbc78c90757
buddhahh <matheushins2015@gmail.com>
2026-05-24 19:36:41 -0300

    models secretaria

--- me_ajuda_app/apps/secretarias/models.py
+++ me_ajuda_app/apps/secretarias/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Secretaria(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    sigla = models.CharField('Sigla', max_length=10)
+    descricao = models.CharField('Descricao', max_length=200)
+    site = models.CharField('Site', max_length=200)
