af3d60ab027f1bca656cfe4ee0d0947fac2ad99c
Tuzinhoo <arthurmentgesdebem@gmail.com>
2026-05-24 22:41:02 -0300

    finalização protocolo urls

--- me_ajuda_app/apps/protocolos/urls.py
+++ me_ajuda_app/apps/protocolos/urls.py
@@ -3,3 +3,10 @@ from . import views
 from rest_framework import routers
 
 app_name = 'protocolos'
+
+router = routers.SimpleRouter()
+router.register('', views.ProtocoloViewSet, basename='protocolos')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
