0bbfcc44ef1d4ac02a3ce124a1c1fa2bea701239
Tuzinhoo <arthurmentgesdebem@gmail.com>
2026-05-24 22:25:06 -0300

    começo de models protocolo

--- me_ajuda_app/apps/protocolos/models.py
+++ me_ajuda_app/apps/protocolos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Protocolo(models.Model):
+    protocolo_numero = models.CharField('Número do Protocolo', max_length=20)
+    gerado_em = models.DateTimeField('Gerado em', auto_now_add=True)
+    prazo = models.DateField('Prazo')
+    ocorrencia = models.OneToOneField('ocorrencias.Ocorrencia', on_delete=models.CASCADE)
