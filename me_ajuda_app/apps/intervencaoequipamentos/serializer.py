f105b844503079290bf3791bd67259050850cf71
buddhahh <matheushins2015@gmail.com>
2026-05-24 22:29:06 -0300

    serializer final

--- me_ajuda_app/apps/intervencaoequipamentos/serializer.py
+++ me_ajuda_app/apps/intervencaoequipamentos/serializer.py
@@ -1,2 +1,8 @@
 from .models import IntervencaoEquipamento
 from rest_framework import serializers
+
+class IntervencaoEquipamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IntervencaoEquipamento
+        fields = '__all__'
+
