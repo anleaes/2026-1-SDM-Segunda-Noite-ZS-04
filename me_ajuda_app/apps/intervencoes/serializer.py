85ae4320adc43d931997c1cf5478b2d4f2044ac1
buddhahh <matheushins2015@gmail.com>
2026-05-24 22:10:24 -0300

    serializer 2 de 2

--- me_ajuda_app/apps/intervencoes/serializer.py
+++ me_ajuda_app/apps/intervencoes/serializer.py
@@ -1,2 +1,7 @@
 from .models import Intervencao
 from rest_framework import serializers
+
+class IntervencaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Intervencao
+        fields = '__all__'
