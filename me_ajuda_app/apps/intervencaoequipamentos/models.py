1594d4aa20a1f2893b114c6471ac57d2cdef89f0
Lucas <lucasso061204@gmail.com>
2026-05-24 22:11:51 -0300

    Adicionados Meta e toString

--- me_ajuda_app/apps/intervencaoequipamentos/models.py
+++ me_ajuda_app/apps/intervencaoequipamentos/models.py
@@ -6,3 +6,11 @@ class IntervencaoEquipamento(models.Model):
     custo_total = models.FloatField('Custo Total')
     intervencao = models.ForeignKey('intervencoes.Intervencao', on_delete=models.CASCADE)
     equipamento = models.ForeignKey('equipamentos.Equipamento', on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'Equipamento da Intervenção'
+        verbose_name_plural = 'Equipamentos das Intervenções'
+        ordering =['id']
+
+    def __str__(self):
+        return  f'{self.intervencao} - {self.equipamento} - {self.horas_usado}h - R${self.custo_total}' 
