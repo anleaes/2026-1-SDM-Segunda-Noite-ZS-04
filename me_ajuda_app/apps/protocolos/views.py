a8a80426c38af2b1cac1c63cd0dc517b70828cc9
Tuzinhoo <arthurmentgesdebem@gmail.com>
2026-05-24 22:37:25 -0300

    finalização protocolo views

--- me_ajuda_app/apps/protocolos/views.py
+++ me_ajuda_app/apps/protocolos/views.py
@@ -3,3 +3,6 @@ from .models import Protocolo
 from rest_framework import viewsets
 from .serializer import ProtocoloSerializer
 # Create your views here.
+class ProtocoloViewSet(viewsets.ModelViewSet):
+    queryset = Protocolo.objects.all()
+    serializer_class = ProtocoloSerializer 
