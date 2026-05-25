a48fbbecdb99f7ff7055b435d9744f022690b042
Lucas <lucasso061204@gmail.com>
2026-05-24 22:09:55 -0300

    Criação da classe IntervencaoEquipamento

--- me_ajuda_app/apps/intervencaoequipamentos/models.py
+++ me_ajuda_app/apps/intervencaoequipamentos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class IntervencaoEquipamento(models.Model):
+    horas_usado = models.IntegerField('Horas Usado')
+    custo_total = models.FloatField('Custo Total')
+    intervencao = models.ForeignKey('intervencoes.Intervencao', on_delete=models.CASCADE)
+    equipamento = models.ForeignKey('equipamentos.Equipamento', on_delete=models.CASCADE)
