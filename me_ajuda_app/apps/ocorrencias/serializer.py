d21e2ae9ac7ab5aea89e282d0603dc136afa3c19
buddhahh <matheushins2015@gmail.com>
2026-05-24 21:45:29 -0300

    serlizer fim

--- me_ajuda_app/apps/ocorrencias/serializer.py
+++ me_ajuda_app/apps/ocorrencias/serializer.py
@@ -1,2 +1,7 @@
 from .models import Ocorrencia
 from rest_framework import serializers
+
+class OcorrenciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ocorrencia
+        fields = '__all__'
