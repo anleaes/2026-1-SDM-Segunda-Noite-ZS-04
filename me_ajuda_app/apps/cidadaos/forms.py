ce43b5bc8eb285727b3811e6f6112fbaf9754db2
buddhahh <matheushins2015@gmail.com>
2026-05-27 19:43:37 -0300

    feat(cidadoes_forms) finalização do forms.py.

--- me_ajuda_app/apps/cidadaos/forms.py
+++ me_ajuda_app/apps/cidadaos/forms.py
@@ -2,3 +2,8 @@ from django import forms
 from django.urls import path
 from .models import Cidadao
 
+class CidadaoForm(forms.ModelForm):
+
+    class Meta:
+        model = Cidadao
+        exclude = ()
