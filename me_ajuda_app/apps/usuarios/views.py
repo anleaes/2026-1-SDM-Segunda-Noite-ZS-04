e5d8d06b7715ac9181dda844c9a651713cd93cf8
Lucas <lucasso061204@gmail.com>
2026-05-27 19:13:39 -0300

    feat(usuarios): Ajuste de importação em views

--- me_ajuda_app/apps/usuarios/views.py
+++ me_ajuda_app/apps/usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from me_ajuda_app.apps.usuarios.forms import UsuarioForm
+from .forms import UsuarioForm, UserForm
 from .models import Usuario
 from rest_framework import viewsets
 from .serializer import UsuarioSerializer
