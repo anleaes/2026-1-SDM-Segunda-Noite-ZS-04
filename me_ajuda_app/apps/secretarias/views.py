445fdfd912694f5f0462fea5b27d5231a90db655
buddhahh <matheushins2015@gmail.com>
2026-05-28 18:34:53 -0300

    feat(secretaria_views)adiçao da  secretarias.

--- me_ajuda_app/apps/secretarias/views.py
+++ me_ajuda_app/apps/secretarias/views.py
@@ -7,3 +7,18 @@ from .serializer import SecretariaSerializer
 class SecretariaViewSet(viewsets.ModelViewSet):
     queryset = Secretaria.objects.all()
     serializer_class = SecretariaSerializer  
+
+    def secretaria_lista(request):
+    
+        secretarias = Secretaria.objects.all().order_by('nome')
+        
+        is_funcionario = False
+        if request.user.is_authenticated:
+            is_funcionario = hasattr(request.user, 'funcionario')
+
+        context = {
+            'secretarias': secretarias,
+            'is_funcionario': is_funcionario,
+        }
+        
+        return render(request, 'secretaria_lista.html', context)
