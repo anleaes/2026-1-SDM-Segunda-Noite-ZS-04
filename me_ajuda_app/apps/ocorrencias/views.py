1fc54a32150571a6e3eadc9d71432d380503ac0a
buddhahh <matheushins2015@gmail.com>
2026-05-28 01:07:46 -0300

    feat(ocorrencias_views)lista de ocorrencia.

--- me_ajuda_app/apps/ocorrencias/views.py
+++ me_ajuda_app/apps/ocorrencias/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Ocorrencia
 from rest_framework import viewsets
 from .serializer import OcorrenciaSerializer
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class OcorrenciaViewSet(viewsets.ModelViewSet):
