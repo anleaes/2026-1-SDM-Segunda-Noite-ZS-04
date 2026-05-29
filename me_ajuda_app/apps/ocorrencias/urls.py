530a10151b61b29de648e93a6bacb928f48aaeeb
buddhahh <matheushins2015@gmail.com>
2026-05-29 20:49:26 -0300

    feat(ocorrencia_urls)adição do botão deletar

--- me_ajuda_app/apps/ocorrencias/urls.py
+++ me_ajuda_app/apps/ocorrencias/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('lista/', views.ocorrencias_lista, name='ocorrencias_lista'),
     path('visualizar/<int:id>/', views.visualizar_ocorrencia, name='visualizar_ocorrencia'),
     path('atualizar-status/<int:ocorrencia_id>/', views.atualizar_status, name='atualizar_status'),
+    path('deletar/<int:id>/', views.ocorrencia_deletar, name='ocorrencia_deletar'),
 ]
