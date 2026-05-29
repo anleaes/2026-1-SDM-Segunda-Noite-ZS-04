11164564a497b2bae56be74dde621772344b9b6b
Lucas <lucasso061204@gmail.com>
2026-05-29 17:29:16 -0300

    feat(template_equipamentos): Início de lista

--- me_ajuda_app/apps/equipamentos/views.py
+++ me_ajuda_app/apps/equipamentos/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from .models import Equipamento
 from rest_framework import viewsets
 from .serializer import EquipamentoSerializer
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class EquipamentoViewSet(viewsets.ModelViewSet):
     queryset = Equipamento.objects.all()
     serializer_class = EquipamentoSerializer  
+
+@login_required(login_url='/usuarios/login/')
+def equipamentos_lista(request):
+    usuario = request.user.usuario
+    is_funcionario = hasattr(usuario, 'funcionario')
+    equipamentos = Equipamento.objects.all().order_by('-id')
+
+    context = {
+        'equipamentos': equipamentos,
+        'is_funcionario': is_funcionario,
+    }
+    
+    return render(request, 'equipamento/equipamentos_lista.html', context)
