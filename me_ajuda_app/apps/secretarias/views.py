8d0536c0bd700efbf2eddd880c3c8a6a10e1ba1e
Lucas <lucasso061204@gmail.com>
2026-05-29 02:13:49 -0300

    fix(template_secretaria): Correção de estilos

--- me_ajuda_app/apps/secretarias/views.py
+++ me_ajuda_app/apps/secretarias/views.py
@@ -12,13 +12,8 @@ def secretaria_lista(request):
 
     secretarias = Secretaria.objects.all().order_by('nome')
     
-    is_funcionario = False
-    if request.user.is_authenticated:
-        is_funcionario = hasattr(request.user, 'funcionario')
-
     context = {
         'secretarias': secretarias,
-        'is_funcionario': is_funcionario,
     }
     
     return render(request, 'secretaria/secretarias_lista.html', context)
