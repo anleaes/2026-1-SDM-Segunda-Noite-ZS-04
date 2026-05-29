bfba5d85053400526eefecb2026dd9a043436e35
Tuzinhoo <arthurmentgesdebem@gmail.com>
2026-05-29 02:55:29 -0300

    intervencao view começo

--- me_ajuda_app/apps/intervencoes/views.py
+++ me_ajuda_app/apps/intervencoes/views.py
@@ -7,3 +7,13 @@ from .serializer import IntervencaoSerializer
 class IntervencaoViewSet(viewsets.ModelViewSet):
     queryset = Intervencao.objects.all()
     serializer_class = IntervencaoSerializer  
+
+    def listar_intervencoes(request):
+    # Busca todas as intervenções do banco de dados
+    # Ordenado pelas mais recentes (ajuste o campo de data se necessário)
+    intervencoes = Intervencao.objects.all().order_by('-id')
+
+    context = {
+        'intervencoes': intervencoes,
+    }
+    return render(request, 'intervencoes/intervencoes_lista.html', context)
