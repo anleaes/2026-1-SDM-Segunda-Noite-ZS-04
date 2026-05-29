a158401ce9428e4929aeb9e3512aa5900f0bbabb
Tuzinhoo <arthurmentgesdebem@gmail.com>
2026-05-29 03:29:54 -0300

    intervencao forms

--- me_ajuda_app/apps/intervencoes/views.py
+++ me_ajuda_app/apps/intervencoes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Intervencao
 from rest_framework import viewsets
 from .serializer import IntervencaoSerializer
+from .forms import IntervencaoForm
 
 # Create your views here.
 class IntervencaoViewSet(viewsets.ModelViewSet):
@@ -17,3 +18,36 @@ class IntervencaoViewSet(viewsets.ModelViewSet):
         'intervencoes': intervencoes,
     }
     return render(request, 'intervencoes/intervencoes_lista.html', context)
+
+def nova_intervencao(request):   
+    if request.method == 'POST':
+        form = IntervencaoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('intervencoes_lista') # Redireciona de volta para a lista
+    else:
+        form = IntervencaoForm()
+
+    context = {
+        'form': form,
+        'acao': 'Nova',
+    }
+    return render(request, 'intervencoes/intervencao_form.html', context)
+
+
+def editar_intervencao(request, id):
+    intervencao = get_object_or_404(Intervencao, id=id)
+
+    if request.method == 'POST':
+        form = IntervencaoForm(request.POST, request.FILES, instance=intervencao)
+        if form.is_valid():
+            form.save()
+            return redirect('intervencoes_lista')
+    else:
+        form = IntervencaoForm(instance=intervencao)
+
+    context = {
+        'form': form,
+        'acao': 'Editar',
+    }
+    return render(request, 'intervencoes/intervencao_form.html', context)
