10d73e00bf9efec01b2c5eb9b1fb7674b5307897
Lucas <lucasso061204@gmail.com>
2026-06-01 17:03:09 -0300

    fix(usuario): Ajuste no Serializer

--- me_ajuda_app/apps/usuarios/serializer.py
+++ me_ajuda_app/apps/usuarios/serializer.py
@@ -1,7 +1,17 @@
 from .models import Usuario
 from rest_framework import serializers
 
+
 class UsuarioSerializer(serializers.ModelSerializer):
+    tipo_usuario = serializers.SerializerMethodField()
+
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ["id", "nome", "sobrenome", "cpf", "email", "user", "tipo_usuario"]
+
+    def get_tipo_usuario(self, obj):
+        if hasattr(obj, "funcionario"):
+            return "funcionario"
+        elif hasattr(obj, "cidadao"):
+            return "cidadao"
+        return "usuario_base"
