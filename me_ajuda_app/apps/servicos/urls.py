377f4f2858fce57858311177b27a289eecef2493
Lucas <lucasso061204@gmail.com>
2026-06-07 18:59:21 -0300

    feat(templates): Implementação de editar/excluir serviços

--- me_ajuda_app/apps/servicos/urls.py
+++ me_ajuda_app/apps/servicos/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('api/', include(router.urls) ),
     path('listar/', views.servicos_lista, name='servicos_lista'),
     path('criar/', views.servico_criar, name='servico_criar'),
-    # path('editar/<int:servico_id>/', views.editar_servico, name='editar_servico'),
-    # path('excluir/<int:servico_id>/', views.excluir_servico, name='excluir_servico'),
+    path('editar/<int:id_servico>/', views.editar_servico, name='editar_servico'),
+    path('excluir/<int:id_servico>/', views.excluir_servico, name='excluir_servico'),
 ]
