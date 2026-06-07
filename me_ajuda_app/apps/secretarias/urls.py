7b74976f700d49563393c8a7a02d1c07e388dbf6
Lucas <lucasso061204@gmail.com>
2026-06-07 19:40:46 -0300

    feat(templates): Implementação de editar/excluir secretarias

--- me_ajuda_app/apps/secretarias/urls.py
+++ me_ajuda_app/apps/secretarias/urls.py
@@ -9,8 +9,8 @@ router.register('', views.SecretariaViewSet, basename='secretarias')
 
 urlpatterns = [
     path('api/', include(router.urls) ),
-    path('listar/', views.secretaria_lista, name='secretarias_lista'),
+    path('listar/', views.secretaria_lista, name='secretaria_lista'),
     path('criar/', views.secretaria_criar, name='secretaria_criar'),
-    # path('editar/<int:secretaria_id>/', views.editar_secretaria, name='editar_secretaria'),
-    # path('excluir/<int:secretaria_id>/', views.excluir_secretaria, name='excluir_secretaria'),
+    path('editar/<int:id_secretaria>/', views.editar_secretaria, name='editar_secretaria'),
+    path('excluir/<int:id_secretaria>/', views.excluir_secretaria, name='excluir_secretaria'),
 ]
