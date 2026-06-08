f873bb364443082f2dafc33305b1aba1be413a0c
Lucas <lucasso061204@gmail.com>
2026-06-07 21:55:29 -0300

    feat(templates): Início de ver/excluir intervencao

--- me_ajuda_app/apps/intervencoes/urls.py
+++ me_ajuda_app/apps/intervencoes/urls.py
@@ -9,13 +9,14 @@ router.register('', views.IntervencaoViewSet, basename='intervencoes')
 
 urlpatterns = [
     path('api/', include(router.urls) ),
-    path('listar/', views.lista_intervencoes, name='intervencoes_lista'),
+    path('listar/', views.lista_intervencoes, name='lista_intervencoes'),
     path('alocacao/', views.alocacao_equipamentos, name='alocacao_equipamentos'),
     path('alocacao/adicionar/<int:equipamento_id>/', views.adicionar_alocacao, name='adicionar_alocacao'),
     path('alocacao/remover/<int:equipamento_id>/', views.excluir_alocacao, name='excluir_alocacao'),
     path('alocacao/editar/<int:equipamento_id>/', views.editar_alocacao, name='editar_alocacao'),
     path('criar/<int:ocorrencia_id>/', views.criar_intervencao, name='criar_intervencao'),
     path('equipamentos/', views.lista_equipamentos_intervencao, name='lista_equipamentos_intervencao'),
-    # path('<int:intervencao_id>/', views.ver_intervencao, name='ver_intervencao'),
-    # path('<int:intervencao_id>/cancelar', views.cancelar_intervencao, name='cancelar_intervencao'),
+    path('<int:intervencao_id>/', views.ver_intervencao, name='ver_intervencao'),
+    path('editar/<int:intervencao_id>/', views.editar_intervencao, name='editar_intervencao'),
+    path('excluir/<int:intervencao_id>/', views.excluir_intervencao, name='excluir_intervencao'),
 ]
