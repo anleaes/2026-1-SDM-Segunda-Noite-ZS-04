d8eaf4b58f1b1ce3c54b0dbef8baad65231a247b
Lucas <lucasso061204@gmail.com>
2026-06-11 13:40:25 -0300

    fix(intervencoes): Ajuste no campo de data

--- me_ajuda_app/apps/intervencoes/forms.py
+++ me_ajuda_app/apps/intervencoes/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Intervencao
 
+
 class IntervencaoForm(forms.ModelForm):
 
     class Meta:
         model = Intervencao
         exclude = ['funcionario', 'ocorrencia']
+
+        widgets = {
+            'data_exec': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'}, format='%Y-%m-%d'
+            ),
+        }
